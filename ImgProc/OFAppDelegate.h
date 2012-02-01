c977e4f5c0111882db004772d9b23afab40206b1
Jamis <jamismanwaring@gmail.com>
2012-01-31 18:19:24 -0700

    Initial Commit

---
+++ ImgProc/OFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OFAppDelegate.h
+//  ImgProc
+//
+//  Created by Jamis Johnson on 1/31/12.
+//  Copyright (c) 2012 University of Utah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImgProc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImgProc
+//
+//  Created by Jamis Johnson on 1/31/12.
+//  Copyright (c) 2012 University of Utah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OFAppDelegate class]));
+    }
+}
